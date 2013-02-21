3affbb1ec8b6f7ea3514b6a579842ce428d2e35d
Yong Wen Chua <yongwen.chua10@imperial.ac.uk>
2013-02-21 20:44:59 +0000

    Untested sphere implemented.

--- source/sphere.cc
+++ source/sphere.cc
@@ -2,21 +2,67 @@
 
 // TODO: sphere class
 
+// Sphere intersection: http://www.csee.umbc.edu/~olano/435f02/ray-sphere.html
 
-Sphere::Sphere()
+Sphere::Sphere(): radius(0)
 {
 
 }
 
 
 
-Sphere::Sphere(Vec3f centre, float radius, Vec3f color)
+Sphere::Sphere(Vec3f centre, float radius, Vec3f colour):
+		centre(centre), radius(radius), colour(colour)
 {
 
 }
-
-
 bool Sphere::intersect(const Ray &r, Hit &h)
 {
+	// See notes Lecture 10 slide 12 for the "normalised" version of the equation we are solving
+	// The quadratic equation is t^2 + bt + c
+	float b,c;	// the coefficients for the quadratic equation we are trying to solve
+	Vec3f delta = r.getOrigin()-centre;
+	// Now let's calculate the coefficients
+	// a = 1.f;
+	b = r.getDirection().Dot3( delta );
+	c = delta.Length() * delta.Length() - radius*radius;
+
+	// Now let's calculate the determinant
+	float determinant = b^2-c;
+
+	// No real roots = no intersection
+	if (determinant < 0)
+		return false;
+
+	//Real roots? Find intersection -- we want the smaller one
+	float t = min( (-1*b + sqrt(determinant)), (-1*b - sqrt(determinant)) );
+	h.set(t, colour);
+
 	return true;
 }
+
+// Getters and Setters
+const Vec3f& Sphere::getCentre() const {
+	return centre;
+}
+
+void Sphere::setCentre(const Vec3f& centre) {
+	this->centre = centre;
+}
+
+const Vec3f& Sphere::getColour() const {
+	return colour;
+}
+
+void Sphere::setColour(const Vec3f& colour) {
+	this->colour = colour;
+}
+
+float Sphere::getRadius() const {
+	return radius;
+}
+
+void Sphere::setRadius(float radius) {
+	this->radius = radius;
+}
+
--- source/sphere.h
+++ source/sphere.h
@@ -9,19 +9,28 @@
 
 class Sphere : public Object3D{
 
+	Vec3f centre, colour;
+	float radius;
 
 public:
 
-  // Default constructor.
-  Sphere();
+	// Default constructor.
+	Sphere();
 
-  // Constructor with parameters specified.
-  Sphere(Vec3f centre, float radius, Vec3f color);
+	// Constructor with parameters specified.
+	Sphere(Vec3f centre, float radius, Vec3f color);
 
-  // A concrete implementation of pure virtual function in parent
-  // class.
-  virtual bool intersect(const Ray &r, Hit &h);
+	// A concrete implementation of pure virtual function in parent
+	// class.
+	virtual bool intersect(const Ray &r, Hit &h);
 
+	// Getters and Setters
+	const Vec3f& getCentre() const;
+	void setCentre(const Vec3f& centre);
+	const Vec3f& getColour() const;
+	void setColour(const Vec3f& colour);
+	float getRadius() const;
+	void setRadius(float radius);
 };
 
 #endif /* SPHERE_H_ */
